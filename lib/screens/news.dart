c0096be90b92cbdc2c02e4e3422f76ec3eb1e872
senseichrollon <42702456+senseichrollon@users.noreply.github.com>
2022-06-13 21:58:40 -0500

    merge

--- lib/screens/news.dart
+++ lib/screens/news.dart
@@ -1,6 +1,9 @@
 // news page
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
 
 class NewsPage extends StatefulWidget {
   const NewsPage({Key key}) : super(key: key);
@@ -12,6 +15,55 @@ class NewsPage extends StatefulWidget {
 class _NewsPageState extends State<NewsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kLightBackgroundColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'News', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)
+          ]
+        ),
+          actions: [
+            IconButton(
+                iconSize: 30,
+                color: kDarkTextColor.withOpacity(0.9),
+                onPressed: () {},
+                tooltip: "Search",
+                icon: Icon(Icons.search))
+          ]
+      ),
+      body: Container(
+        child: Column(
+            children: <Widget>[
+              // categories
+              Row(
+                children: <Widget>[
+                  TextButton(
+                    onPressed: (){},
+                    child: Text('All'),
+                  ),
+
+                ],
+
+              ),
+            ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: <Widget>[
+
+        ],
+      ),
+    );
   }
 }
