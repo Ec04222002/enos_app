d1ac1a253a1566493ec3831ad1a63198dc95e0d4
senseichrollon <42702456+senseichrollon@users.noreply.github.com>
2022-06-15 21:23:19 -0500

    basic design stuff

--- lib/screens/news.dart
+++ lib/screens/news.dart
@@ -1,5 +1,6 @@
 // news page
 
+import 'package:enos/screens/search.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,17 +12,13 @@ class NewsPage extends StatefulWidget {
   @override
   State<NewsPage> createState() => _NewsPageState();
 }
-
+List<CategoryTile> categories = [];
 class _NewsPageState extends State<NewsPage> {
-
-  List<CategoryTile> categories = [];
 
   void initState() {
     super.initState();
     categories = getCategories();
-    print('urmom');
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +36,9 @@ class _NewsPageState extends State<NewsPage> {
             IconButton(
                 iconSize: 30,
                 color: kDarkTextColor.withOpacity(0.9),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> SearchPage()));
+                },
                 tooltip: "Search",
                 icon: Icon(Icons.search))
           ]
@@ -47,11 +46,16 @@ class _NewsPageState extends State<NewsPage> {
       body: Container(
         child: Column(
             children: <Widget>[
-              Container(
+              Expanded(
                 child: ListView.builder(
+                  shrinkWrap: true,
+
+                  scrollDirection: Axis.horizontal,
                   itemCount: categories.length,
+                  padding: EdgeInsets.all(10),
                   itemBuilder: (context,index) {
-                    return CategoryTile(categories[index].text, categories[index].selected);
+                    print('$index boo');
+                    return CategoryTile(categories[index].text, categories[index].selected,index);
                   },
                 ),
               )
@@ -65,21 +69,20 @@ class _NewsPageState extends State<NewsPage> {
 List<CategoryTile> getCategories() {
   List<CategoryTile> ret = [];
 
-  CategoryTile tile = CategoryTile("All", true);
+  CategoryTile tile = CategoryTile("All", true,0);
   ret.add(tile);
 
-  tile = CategoryTile("Stock", false);
+  tile = CategoryTile("Stock", false,1);
   ret.add(tile);
 
-  tile = CategoryTile("NFT", false);
+  tile = CategoryTile("NFT", false,2);
   ret.add(tile);
 
-  tile = CategoryTile("Crypto", false);
+  tile = CategoryTile("Crypto", false,3);
   ret.add(tile);
 
-  tile = CategoryTile("Tech", false);
+  tile = CategoryTile("Tech", false,4);
   ret.add(tile);
-  print('ur mom gay');
   return ret;
 }
 
@@ -88,24 +91,37 @@ class CategoryTile extends StatefulWidget {
   @override
   String text;
   bool selected = false;
+  int index;
 
-  CategoryTile(String text, bool inSelect) {
+
+  CategoryTile(String text, bool inSelect, int index) {
     this.text = text;
     selected = inSelect;
+    this.index = index;
   }
+
   State<CategoryTile> createState() => _CategoryTileState();
 }
 
 class _CategoryTileState extends State<CategoryTile> {
+  static int cSelect = 0;
+
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: this.widget.selected? Colors.lightBlueAccent:Colors.blue,
+      margin: EdgeInsets.all(5),
+      color: Colors.blue,
       child: TextButton(
+        
         child: Text(this.widget.text, style: TextStyle(color: Colors.white,fontSize: 18),),
         onPressed: () {
           setState(() {
             this.widget.selected = true;
+            categories[cSelect].selected = false;
+            cSelect = this.widget.index;
+            print(cSelect);
           });
         },
       ),
