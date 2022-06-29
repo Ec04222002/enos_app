5a98111d2ebf92e240ee915fee3e26bfb241a670
senseichrollon <42702456+senseichrollon@users.noreply.github.com>
2022-06-29 11:24:30 -0500

    Create Profile Pic

---
+++ lib/widgets/ProfilePic.dart
@@ -0,0 +1,56 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePicture extends StatelessWidget {
+
+  Image image = null;
+  String name;
+  List<Color> colors = [Colors.red,Colors.blue,Colors.grey, Colors.green, Colors.orange,Colors.purple,Colors.black,Colors.yellow];
+  var rng = Random();
+  int pos;
+  ProfilePicture({this.image,this.name}) {
+    pos = rng.nextInt(colors.length);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+
+
+  Widget hasImage() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+            image: image.image,
+            fit: BoxFit.cover
+        )
+      ),
+    );
+  }
+
+  Widget noImage() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      alignment: Alignment.center,
+      child: Text(
+        name.substring(0,2),
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+      decoration: BoxDecoration(
+        color: colors[pos],
+        shape: BoxShape.circle
+      ),
+    );
+  }
+}
